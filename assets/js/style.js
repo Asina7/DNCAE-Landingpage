ad80a40165c61fe5c37de33d1fe70e461508040f
Asina7 <asina.abdulla@wisoft.com>
2023-01-07 08:39:59 +0400

    Initial commit

---
+++ assets/js/style.js
@@ -0,0 +1,80 @@
+/* Please ❤ this if you like it! */
+
+
+(function ($) {
+    "use strict";
+
+    $(function () {
+        var header = $(".start-style");
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 10) {
+                header.removeClass('start-style').addClass("scroll-on");
+            } else {
+                header.removeClass("scroll-on").addClass('start-style');
+            }
+        });
+    });
+
+    //Animation
+    $(".navbar-nav .nav-menu li a, a.scrool").on('click', function (e) {
+
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+        var target_offset = $("#" + trgt).offset();
+        var target_top = target_offset.top;
+
+        $('html,body').animate({
+            scrollTop: target_top - 70
+        }, 1000);
+        return false;
+
+    });
+
+    $(".EnquireNow").on('click', function (e) {
+
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+        var target_offset = $("#" + trgt).offset();
+        var target_top = target_offset.top;
+
+        $('html,body').animate({
+            scrollTop: target_top - 120
+        }, 1000);
+        return false;
+
+    });
+
+
+    $(document).ready(function () {
+        $('body.hero-anime').removeClass('hero-anime');
+    });
+
+    //Menu On Hover
+
+    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        if ($(window).width() > 750) {
+            var _d = $(e.target).closest('.nav-item');
+            _d.addClass('show');
+            setTimeout(function () {
+                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 1);
+        }
+    });
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("dark")) {
+            $("body").removeClass("dark");
+            $("#switch").removeClass("switched");
+        } else {
+            $("body").addClass("dark");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
